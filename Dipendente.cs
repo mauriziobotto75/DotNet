b8b0650038e6c5884b774cc2bdfbc366bfa062bc
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2021-09-16 08:16:35 +0200

    Update Dipendente.cs

--- Dipendente.cs
+++ Dipendente.cs
@@ -28,3 +28,40 @@ class Dipendente
                return string.Format("{0} {1} {2} guadagna {3} Euro", nome, cognome, this.GetType().namespace, this.CalcolaStipendio());
         }
     }
+class Quadro : Dipendente
+{
+  protected int premio;
+  public Quadro(string nome, string cognome, int ore, int pagaOraria, int premio) : base(nome, cognome, ore, pagaOraria)
+  {
+     this.premio = premio;
+  }
+  protected override int calcolaStipendio()
+  {
+     return base.CalcolaStipendio() + premio;
+     }
+  public override Dipendente Promuovi()
+  {
+     return new Direttore(nome, cognome, ore, pagaOraria, 500);
+     }
+   public string Stampa()
+   {
+      return string.Format("{0} {1} {2} guadagna {3} Euro", nome, cognome, this.GetType().Name, this.CalcolaStipendio());
+   }
+   
+ }
+ class Direttore : Quadro
+ {
+     public Direttore(string nome, string Cognome, int ore, int pagaOraria, int premio) : base(nome, cognome, ore, pagaOraria, premio)
+     {
+     }
+     protected override int calcolaStipendio()
+     {
+        return base.calcolaStipendio() + (2 * premio);
+     }
+     
+     public string Stampa()
+     {
+        return string.Format("{0} {1} {2} guadagna {3} Euro", nome, cognome, this.GetType().Name, this.CalcolaStipendio());
+     }
+ }
+
