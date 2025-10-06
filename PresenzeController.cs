c38d218b39f92de47e99cb7179a2c0d4c3506dcb
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2025-10-06 10:32:11 +0200

    Update Rilevazione Presenze con utilizzo di MVC

--- PresenzeController.cs
+++ PresenzeController.cs
@@ -1,17 +1,1 @@
-using System;
-using System.Linq;
-using System.Web.Mvc;
-
-public class PresenzeController : Controller
-{
-    private ApplicationDbContext db = new ApplicationDbContext();
-
-    public ActionResult Calendario(int idAnagrafica, int mese, int anno)
-    {
-        var presenze = db.Presenze
-            .Where(p => p.ID_Anagrafica == idAnagrafica && p.Data.Month == mese && p.Data.Year == anno)
-            .ToList();
-
-        return View(presenze);
-    }
-}
+// PresenzeController.cs - placeholder content
