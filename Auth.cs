066717020bd5148e3efcefc9a26ecee8ba6ae578
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-04-17 10:31:08 +0200

    Refactor HasDiritto method for SQL query optimization
    Aggiornamento del metodo HasDiritto per ottimizzazione di SQL query

--- Auth.cs
+++ Auth.cs
@@ -7,20 +7,21 @@ public class Auth
     }
 
     public static bool HasDiritto(string codice)
+{
+    using (SqlConnection cn = Db.Conn())
     {
-        using (SqlConnection cn = Db.Conn())
-        {
-            SqlCommand cmd = new SqlCommand(@"
-            SELECT COUNT(*) FROM Ruoli_Diritti RD
-            INNER JOIN DIRITTI D ON RD.IdDiritto = D.IdDiritto
-            WHERE RD.IdRuolo=@r AND D.Codice=@c", cn);
+        SqlCommand cmd = new SqlCommand(@"
+        SELECT COUNT(*)
+        FROM DIRITTI D
+        INNER JOIN RUOLI_DIRITTI RD ON D.IdDiritto = RD.IdDiritto
+        WHERE RD.IdRuolo = @r AND D.Codice = @c", cn);
 
-            cmd.Parameters.AddWithValue("@r",
-                HttpContext.Current.Session["IdRuolo"]);
-            cmd.Parameters.AddWithValue("@c", codice);
+        cmd.Parameters.AddWithValue("@r",
+            HttpContext.Current.Session["IdRuolo"]);
+        cmd.Parameters.AddWithValue("@c", codice);
 
-            cn.Open();
-            return (int)cmd.ExecuteScalar() > 0;
-        }
+        cn.Open();
+        return (int)cmd.ExecuteScalar() > 0;
     }
+}
 }
