2fb06339e846e4624a19402673bd6691759d50e4
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-04-21 15:02:00 +0200

    Add PersoneController to manage person data
    Sviluppo del sito di ForteChance in versione MVC

---
+++ PersoneControllers.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+ using System.Web.Mvc;
+  using System.Data.SqlClient;
+   using PoloFormativo.Mvc.Infrastructure;
+public class PersoneController:Controller{ [RequireDiritto("GEST_PERSONE")] 
+public ActionResult Index()
+{var l=new List<dynamic>(); 
+using(SqlConnection cn=Db.Conn())
+{ 
+    SqlCommand cmd=new SqlCommand("SELECT IdPersona,Nome,Cognome FROM Persone",cn); 
+    cn.Open(); 
+    var dr=cmd.ExecuteReader(); 
+    while(dr.Read())
+     l.Add(new{IdPersona=dr[0],Nome=dr[1],Cognome=dr[2]});
+      } 
+      return View(l);
+      } 
+      }
