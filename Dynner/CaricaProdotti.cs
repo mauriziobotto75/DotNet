0c95637b947b226b96c6e5f656d681b764afd92b
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-05-29 11:13:32 +0200

    Add CaricaProdotti method to load products
    Caricamento prodotti (riquadro destro)

---
+++ Dynner/CaricaProdotti.cs
@@ -0,0 +1,21 @@
+private void CaricaProdotti(int menuId)
+{
+    var prodotti = _db.MenuProdotti
+        .Where(mp => mp.MenuId == menuId)
+        .Select(mp => mp.Prodotto)
+        .ToList();
+
+    pnlProdotti.Controls.Clear();
+
+    foreach (var p in prodotti)
+    {
+        var btn = new Button();
+        btn.Text = $"{p.Nome}\n{p.Prezzo:C}";
+        btn.Width = 120;
+        btn.Height = 80;
+
+        btn.Click += (s, e) => AggiungiProdotto(p);
+
+        pnlProdotti.Controls.Add(btn);
+    }
+}
