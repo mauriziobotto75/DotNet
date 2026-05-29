a0a219735c9193e22034f8127be8a5c11911d130
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-05-29 15:08:35 +0200

    Add new properties to Menu class
    Sviluppo per esteso della gestione del menu

--- Dynner/Menu.cs
+++ Dynner/Menu.cs
@@ -1,8 +1,15 @@
-public class Menu
+ public class Menu
 {
     public int Id { get; set; }
     public string Nome { get; set; }
 
+    public decimal Prezzo { get; set; }
+    public decimal PrezzoImposto { get; set; }
+
+    public bool StampaDettaglio { get; set; }
+    public int CodiceIva { get; set; }
+
     public virtual ICollection<MenuProdotto> Prodotti { get; set; }
 }
+``
 
