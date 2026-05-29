149bd19f1a3754e3086832145edf580e63dd1278
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-05-29 15:10:14 +0200

    Add Ordine and Categoria properties to MenuProdotto
    Gestione Menu' Prodotto

--- Dynner/MenuProdotto.cs
+++ Dynner/MenuProdotto.cs
@@ -1,10 +1,13 @@
 
-public class MenuProdotto
+ public class MenuProdotto
 {
     public int Id { get; set; }
 
     public int MenuId { get; set; }
     public int ProdottoId { get; set; }
+
+    public int Ordine { get; set; } // posizione nel menu
+    public string Categoria { get; set; } // Antipasto, Primi, ecc.
 
     public virtual Menu Menu { get; set; }
     public virtual Prodotto Prodotto { get; set; }
