0ddfda2fff35a43e91d35dd8766fd663e45682d4
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-06-03 12:07:57 +0200

    Fetch comanda with related products from database
    Aggiunta comanda con i prodotti relativi

--- Dynner/Forms/FormConto.cs
+++ Dynner/Forms/FormConto.cs
@@ -1,3 +1,7 @@
+var comanda = db.Comande
+    .Include("Comande_Dettaglio.Prodotti")
+    .First(c => c.Id == id);
+
 private int _comandaId;
 private RistorazioneEntities _db = new RistorazioneEntities();
 
