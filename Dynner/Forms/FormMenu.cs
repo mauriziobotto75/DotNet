2e2c2bde1e5e16003d26da6130a58b18f1d08cee
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-06-03 12:09:06 +0200

    Fetch menu with related products from the database
    Fetch del menu con prodotti relativi

--- Dynner/Forms/FormMenu.cs
+++ Dynner/Forms/FormMenu.cs
@@ -1,3 +1,7 @@
+var menu = db.Menu
+    .Include("Menu_Prodotti.Prodotti")
+    .First(m => m.Id == id);
+
 public event Action<Prodotti> OnProdottoSelezionato;
 private void btnProdotto_Click(object sender, EventArgs e)
 {
