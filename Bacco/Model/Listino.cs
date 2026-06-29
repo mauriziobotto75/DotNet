2eb1d35c0cd02adeedcc51b087a74e2e81645eae
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-06-29 16:08:46 +0200

    Create Listino.cs
    Creazione classe Listino

---
+++ Bacco/Model/Listino.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Listino
+{
+    public int Id { get; set; }
+    public string TipoCamera { get; set; }
+    public DateTime DataInizio { get; set; }
+    public DateTime DataFine { get; set; }
+    public decimal Prezzo { get; set; }
+}
