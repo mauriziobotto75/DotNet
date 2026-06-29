426140b21e71cc974ad0c6d82ad15af5bbc80909
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-06-29 15:56:56 +0200

    Create Camera.cs
    Aggiunta della classe base Camera

---
+++ Bacco/Model/Camera.cs
@@ -0,0 +1,8 @@
+public class Camera : BaseModel
+{
+    public int Id { get; set; }
+    public string Numero { get; set; }
+    public string Tipo { get; set; }
+    public decimal Prezzo { get; set; }
+    public string Stato { get; set; }
+}
