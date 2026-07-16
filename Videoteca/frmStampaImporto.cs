857976a4df9c5bf45303cfba7f167b5911effb4e
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-07-16 15:51:04 +0200

    Add CalcolaImporto method to calculate rental cost
    Gestione della videata di stampa prezzo Noleggio

--- Videoteca/frmStampaImporto.cs
+++ Videoteca/frmStampaImporto.cs
@@ -8,3 +8,13 @@ public decimal CalcolaImporto(
 
     return giorni * costoGiornaliero;
 }
+public decimal CalcolaImporto(
+    DateTime dataNoleggio,
+    DateTime dataReso,
+    decimal costoGiornaliero)
+{
+    int giorni =
+        (dataReso - dataNoleggio).Days + 1;
+
+    return giorni * costoGiornaliero;
+}
