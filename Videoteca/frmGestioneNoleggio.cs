87113e8f99a0db42b9b8c73983f1a6aee775e893
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-07-16 15:42:42 +0200

    Implement video rental method in frmGestioneNoleggio
    Add NoleggiaVideo method for video rental processing.

--- Videoteca/frmGestioneNoleggio.cs
+++ Videoteca/frmGestioneNoleggio.cs
@@ -25,4 +25,69 @@ public bool Disponibile(
         return false;
 
     return Convert.ToInt32(obj) > 0;
+}   // funzione di gestione del noleggio  nel FORM Gestione Noleggio
+public bool NoleggiaVideo(
+    int cliente,
+    int video,
+    int formato)
+{
+    SqlConnection cn = new DBConnection().Connection;
+
+    cn.Open();
+
+    SqlTransaction tr =
+        cn.BeginTransaction();
+
+    try
+    {
+        SqlCommand cmd1 =
+            new SqlCommand(
+            @"INSERT INTO tbmovvideo
+              (
+                kscliente,
+                ksvideo,
+                ksformato,
+                datanoleggio
+              )
+              VALUES
+              (
+                @cliente,
+                @video,
+                @formato,
+                GETDATE()
+              )",
+            cn, tr);
+
+        cmd1.Parameters.AddWithValue("@cliente", cliente);
+        cmd1.Parameters.AddWithValue("@video", video);
+        cmd1.Parameters.AddWithValue("@formato", formato);
+
+        cmd1.ExecuteNonQuery();
+
+        SqlCommand cmd2 =
+            new SqlCommand(
+            @"UPDATE tbcopie
+              SET disponibili = disponibili - 1,
+                  noleggiate = noleggiate + 1
+              WHERE ksvideoc=@video
+              AND ksformatoc=@formato",
+            cn, tr);
+
+        cmd2.Parameters.AddWithValue("@video", video);
+        cmd2.Parameters.AddWithValue("@formato", formato);
+
+        cmd2.ExecuteNonQuery();
+
+        tr.Commit();
+
+        cn.Close();
+
+        return true;
+    }
+    catch
+    {
+        tr.Rollback();
+        cn.Close();
+        return false;
+    }
 }
