dd7cfd4a80e7c4d1c74b3781bef4075f7eabb907
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-07-27 12:31:29 +0200

    Add PDF generation functionality to Stampa_Testo_Sollecito
    Gestione pulsante stampa PDF del sollecito

--- Amministratori_Condominio/Data/GestioneCausali/Data/Stampa_Testo_Sollecito.cs
+++ Amministratori_Condominio/Data/GestioneCausali/Data/Stampa_Testo_Sollecito.cs
@@ -33,3 +33,33 @@ L'Amministratore";
 
     rtfTesto.Text = testo;
 }
+private void btnPDF_Click(
+    object sender,
+    EventArgs e)
+{
+    SaveFileDialog dlg =
+        new SaveFileDialog();
+
+    dlg.Filter =
+        "File PDF (*.pdf)|*.pdf";
+
+    dlg.FileName =
+        "Sollecito_" +
+        _idRata +
+        ".pdf";
+
+    if(dlg.ShowDialog()
+       == DialogResult.OK)
+    {
+        PdfSollecito.GeneraPDF(
+            dlg.FileName,
+            txtDestinatario.Text,
+            txtIndirizzo.Text,
+            Convert.ToDecimal(
+                txtImporto.Text),
+            rtfTesto.Text);
+
+        MessageBox.Show(
+            "PDF generato correttamente.");
+    }
+}
