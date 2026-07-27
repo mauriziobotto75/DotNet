6b51075b9dd8a240dc880d0044ce1085e440c81a
Maurizio Botto <74532123+mauriziobotto75@users.noreply.github.com>
2026-07-27 09:30:41 +0200

    Add checkboxes and buttons to frmAutocomposizioneSpesa
    Creazione Designer del form Autocomposizione Spesa

---
+++ Amministratori_Condominio/Data/GestioneCausali/Data/frmAutocomposizioneSpesa.Designer.cs
@@ -0,0 +1,13 @@
+CheckBox chkTabelleMillesimali;
+CheckBox chkMetriQuadri;
+CheckBox chkAPersona;
+CheckBox chkContatori;
+CheckBox chkMetriCubi;
+CheckBox chkVari;
+CheckBox chkFattoriMoltiplicativi;
+CheckBox chkAPorta;
+CheckBox chkRipartizioneMista;
+
+Button btnAvanti;
+Button btnIndietro;
+Button btnAbbandona;
